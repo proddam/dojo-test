62a1b492ed8a63afa593ba3c2c27b4ca862c2aba
Younghan <younghan@coinone.com>
2018-06-07 07:20:36 +0000

    fix task input

--- ci/tasks/hello-world/task.sh
+++ ci/tasks/hello-world/task.sh
@@ -2,5 +2,11 @@ set -e
 
 if [[ -d /etc ]]; then
   ls /etc > output/etc.filelisting.txt
+  echo +++++++++++++
+  pwd
+  echo -------------
+  ls -la output
+  echo +++++++++++++
+  cat output/etc.filelisting.txt
 fi
 
